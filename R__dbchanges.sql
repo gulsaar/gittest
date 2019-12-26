ceb184c7d69279c01d6f754032f4bd2b7d3722a5
gulsaar <58323424+gulsaar@users.noreply.github.com>
2019-12-26 17:26:20 +0530

    Update R__dbchanges.sql

--- R__dbchanges.sql
+++ R__dbchanges.sql
@@ -1,5 +1,5 @@
 -- script for release 9 -- dated 
-insert into department values (7,'Science',1046);
+insert into DEPARTMENT values (7,'Science',1046);
 
-insert into department values (8,'Science',1047);
-insert into department values (11,'Science',1080);
+--insert into department values (8,'Science',1047);
+--insert into department values (11,'Science',1080);
