a574097751f9acd22ee721e4c3426a5d4b0214f8
gulsaar <58323424+gulsaar@users.noreply.github.com>
2019-12-26 17:17:14 +0530

    Update R__dbchanges.sql

--- R__dbchanges.sql
+++ R__dbchanges.sql
@@ -1,10 +1,5 @@
 -- script for release 9 -- dated 
---insert into department values (7,'Science',1046);
+insert into department values (7,'Science',1046);
 
---insert into department values (8,'Science',1047);
-
--- Changes for Release 10 date 26th Dec...
-
---delete from department where id in (7,8);
-
+insert into department values (8,'Science',1047);
 insert into department values (11,'Science',1080);
